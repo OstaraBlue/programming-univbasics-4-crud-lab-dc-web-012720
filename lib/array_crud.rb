343f9fbd7986fa5097177c98a2af1cd39ead0f71
Sonata Arrington <sarrington1009@gmail.com>
2020-01-09 19:24:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ary = ["one", "two", "three", "four"]
-  ary.push ("arrays!")
+  ary.push ("five")
 end
 
 def add_element_to_start_of_array(array, element)
