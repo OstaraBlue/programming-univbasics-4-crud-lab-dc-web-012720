fea70409d161b885524c493c22a231a67ca8a118
Sonata Arrington <sarrington1009@gmail.com>
2020-01-09 19:44:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ary = ["wow", "one", "two", "three", "four" "arrays!"]
+  ary.pop
 end
 
 def remove_element_from_start_of_array(array)
