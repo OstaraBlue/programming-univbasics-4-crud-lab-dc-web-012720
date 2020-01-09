d480d8ed77f0d7ecd26f5a23fbe3e42b37274119
Sonata Arrington <sarrington1009@gmail.com>
2020-01-09 19:50:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  ary = ["wow", "one", "two", "three", "four", "arrays!"]
+  ary.[0]
 end
 
 def retrieve_last_element_from_array(array)
